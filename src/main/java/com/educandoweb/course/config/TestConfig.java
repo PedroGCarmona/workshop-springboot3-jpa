242e79fe5b6d57ecc2529d8f34e3a18301130b52
PedroGCarmona <pgcarmona@outlook.com>
2023-07-18 22:31:41 -0300

    teste

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -26,7 +26,8 @@ public class TestConfig implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456"); 
-		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
+		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+		User u3 = new User(null, "Pedro Carmona", "pgabcarmona@gmail.com", "998630663", "123456"); 
 		
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1); 
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2); 

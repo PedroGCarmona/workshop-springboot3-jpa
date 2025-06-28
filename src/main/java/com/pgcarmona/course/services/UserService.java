b34988e220565c2ea1e7f67fa3f5fda3b06fd2d8
PedroGCarmona <pgcarmona@outlook.com>
2025-06-28 16:11:02 -0300

    Service layer, component registration

---
+++ src/main/java/com/pgcarmona/course/services/UserService.java
@@ -0,0 +1,25 @@
+package com.pgcarmona.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pgcarmona.course.entities.User;
+import com.pgcarmona.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired 
+    UserRepository userRepository;
+	
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id){
+        return userRepository.findById(id).get();
+    }
+}
